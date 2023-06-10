355f7b468435b20ae7911bb4fecff48a654dadc0
Advait Desai <advaitcfc@gmail.com>
2023-06-10 16:55:08 +0530

    Added repo

--- src/main/java/com/advait/graphqlapi/controller/EmployeeController.java
+++ src/main/java/com/advait/graphqlapi/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.advait.graphqlapi.controller;
 
 import com.advait.graphqlapi.model.Employee;
+import com.advait.graphqlapi.model.EmployeeInfo;
+import com.advait.graphqlapi.repo.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -15,13 +18,17 @@ import java.util.ArrayList;
 @Controller
 public class EmployeeController {
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     @QueryMapping
     public Flux<Employee> listEmployees(@Argument Integer page, @Argument Integer size) {
         return Flux.fromIterable(new ArrayList<Employee>());
     }
 
     @MutationMapping
-    public Mono<Employee> addEmployee(@Argument boolean exEmployee) {
-        return Mono.just(new Employee());
+    public Mono<Employee> addEmployee(@Argument EmployeeInfo employeeInfo) {
+        Employee employee = new Employee(null, employeeInfo.getName(), employeeInfo.getAddressLine1(), employeeInfo.getAddressLine2(), employeeInfo.getContact(), employeeInfo.getDepartment(), employeeInfo.getSalary(), false);
+        return Mono.just(employeeRepository.save(employee));
     }
 }
---
+++ src/main/java/com/advait/graphqlapi/repo/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.advait.graphqlapi.repo;
+
+import com.advait.graphqlapi.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
